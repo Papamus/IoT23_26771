bdb5fa3e60fe3eb49f1904980a537eb0aa9dfe84
Adam Kusiakiewicz <adam.kusiakiewicz@gmail.com>
2023-12-14 11:51:15 +0100

    Update commit

--- lab3V2/PeopleFn.cs
+++ lab3V2/PeopleFn.cs
@@ -1,29 +1,46 @@
-using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-
+using System.Net;
+using System.Text.Json;
 namespace CdvAzure.Functions
 {
-    public class PeopleFn
+    public class PeopleFN
     {
         private readonly ILogger _logger;
 
-        public PeopleFn(ILoggerFactory loggerFactory)
+        private readonly PeopleService peopleService;
+
+        public PeopleFN(ILoggerFactory loggerFactory, PeopleService peopleService)
         {
-            _logger = loggerFactory.CreateLogger<PeopleFn>();
+            _logger = loggerFactory.CreateLogger<PeopleFN>();
+            this.peopleService = peopleService;
         }
 
-        [Function("PeopleFn")]
+        [Function("PeopleFN")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
-
+            switch  (req.Method){
+                case "POST":
+                    StreamReader reader = new StreamReader(req.Body, System.Text.Encoding.UTF8);
+                    var json = reader.ReadToEnd();
+                    var person = JsonSerializer.Deserialize<Person>(json);
+                    var res =  peopleService.Add(person.FirstName, person.LastName);
+                    response.WriteAsJsonAsync(res);
+                    break;
+                case "PUT":
+                    break;
+                case "GET":
+                    var people = peopleService.Get();
+                    response.WriteAsJsonAsync(people);
+                    break;
+                case "DELETE":
+                    break;    
+            }
             return response;
         }
     }
